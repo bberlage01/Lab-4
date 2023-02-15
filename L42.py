dc95986b0d6dd82fd433eceee37cbdbf33302f80
Bethany Berlage <123026515+bberlage01@users.noreply.github.com>
2023-02-15 15:51:02 -0500

    Add files via upload

---
+++ L42.py
@@ -0,0 +1,12 @@
+#Grace Anspach and Bethany Berlage
+import turtle
+jack=turtle.Turtle()
+jack.color("yellow")
+
+for side in range(4):
+    if side==1:
+        jack.color("blue")
+    if side==3:
+        jack.color("green")
+    jack.forward(100)
+    jack.right(90)
---
+++ L44.py
@@ -0,0 +1,8 @@
+#Grace Anspach and Bethany Berlage
+for x in ["paper","rock","paper","scissor","rock","rock","scissor","paper"]:
+    if x=="paper":
+        print("paper eats rock")
+    if x=="scissors":
+        print("scissors beats paper")
+    if x=="rock":
+        print("rock hits scissors")
